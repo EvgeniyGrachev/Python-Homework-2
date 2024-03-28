2ec924ac49318ea50322c79c59b8517cec3b1de7
Evgeniy <zzz12@list.ru>
2024-03-28 18:47:04 +0300

    Homework_6

---
+++ Homework_6/Ex_4.py
@@ -0,0 +1,18 @@
+# Задание 4. 
+# Напишите функцию в шахматный модуль. 
+# Используйте генератор случайных чисел для случайной расстановки ферзей в задаче выше. 
+# Проверяйте различный случайные варианты и выведите 4 успешных расстановки.
+
+
+import random
+from Ex_3 import validate_queens
+
+def generate_positions():
+    positions = list(range(1, 9))  # создаём список с числами от 1 до 8
+    for i in range(4):  # 4 успешные расстановки
+        random.shuffle(positions)  # перемешиваем список
+        while not validate_queens(positions):  # если расстановка не успешная, перемешиваем ещё раз
+            random.shuffle(positions)
+        print(positions)  # выводим успешную расстановку
+
+print(generate_positions())
